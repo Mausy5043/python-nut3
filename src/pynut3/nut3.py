d539abe50dc24369b3157de1410d18d263825b90
Maurice (mausy5043) Hendrix <Mausy5043@users.noreply.github.com>
2022-11-03 21:04:07 +0100

    bump version

--- src/pynut3/nut3.py
+++ src/pynut3/nut3.py
@@ -29,7 +29,7 @@ import socket
 from telnetlib import Telnet
 from typing import Any, Dict, List, Optional
 
-__version__ = '0.0.1'
+__version__ = '0.0.4'
 __all__ = ['PyNUT3Error', 'PyNUT3Client']
 
 _LOGGER = logging.getLogger(__name__)
